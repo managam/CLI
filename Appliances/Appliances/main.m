0879a4c0e0b481b5ffb8ac323fa2452cf37adae0
Managam Silalahi <managam.silalahi@gmail.com>
2016-09-03 20:55:52 +0700

    Add project Appliances

---
+++ Appliances/Appliances/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Appliances
+//
+//  Created by Managam Silalahi on 9/3/16.
+//  Copyright © 2016 Managam Silalahi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
