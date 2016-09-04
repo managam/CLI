aefa84a187c3801c1edd06955876bd63adc9d5e7
Managam Silalahi <managam.silalahi@gmail.com>
2016-09-04 20:01:27 +0700

    Add project Callbacks and implement run loop

---
+++ Callbacks/Callbacks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Callbacks
+//
+//  Created by Managam Silalahi on 9/4/16.
+//  Copyright © 2016 Managam Silalahi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        // NSLog(@"Hello, World!");
+        [[NSRunLoop currentRunLoop] run];
+    }
+    return 0;
+}
