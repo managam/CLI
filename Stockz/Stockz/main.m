28244e2002b0445b83cf0939c78ebee68f1d9e10
Managam Silalahi <managam.silalahi@gmail.com>
2016-09-05 19:40:32 +0700

    Add project Stockz: Chapeter 30 Property List

---
+++ Stockz/Stockz/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Stockz
+//
+//  Created by Managam Silalahi on 9/5/16.
+//  Copyright © 2016 Managam Silalahi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
