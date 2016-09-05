556a7f2ce124178c3e823729d5981f64dd9db260
Managam Silalahi <managam.silalahi@gmail.com>
2016-09-05 19:53:54 +0700

    Finish property list on project Stockz

--- Stockz/Stockz/main.m
+++ Stockz/Stockz/main.m
@@ -11,7 +11,27 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        // NSLog(@"Hello, World!");
+        
+        NSMutableArray *stocks = [[NSMutableArray alloc] init];
+        
+        NSMutableDictionary *stock;
+        
+        stock = [NSMutableDictionary dictionary];
+        [stock setObject:@"AAPL" forKey:@"symbol"];
+        [stock setObject:[NSNumber numberWithInteger:200] forKey:@"shares"];
+        [stocks addObject:stock];
+        
+        stock = [NSMutableDictionary dictionary];
+        [stock setObject:@"GOOG" forKey:@"symbol"];
+        [stock setObject:[NSNumber numberWithInteger:160] forKey:@"shares"];
+        [stocks addObject:stock];
+        
+        [stocks writeToFile:@"/tmp/stocks.plist" atomically:YES];
+        
+        for (NSDictionary *d in stocks) {
+            NSLog(@"I have %@ shares of %@", [d valueForKey:@"shares"], [d valueForKey:@"symbol"]);
+        }
     }
     return 0;
 }
