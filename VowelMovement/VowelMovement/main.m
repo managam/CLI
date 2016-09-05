a397f089e798fb031b76b90ebf3676fdc496411b
Managam Silalahi <managam.silalahi@gmail.com>
2016-09-05 19:13:51 +0700

    Declaring and composing block

--- VowelMovement/VowelMovement/main.m
+++ VowelMovement/VowelMovement/main.m
@@ -21,6 +21,23 @@ int main(int argc, const char * argv[]) {
         NSMutableArray *devowelizedStrings = [NSMutableArray array];
         // Create a list of characters to be removed from the string
         NSArray *vowels = @[@"a", @"e", @"i", @"o", @"u"];
+        
+        // Declaring the block variable
+        void (^devowelizer)(id, NSUInteger, BOOL *);
+        
+        // Compose a block and assign it to variable
+        devowelizer = ^(id string, NSUInteger i, BOOL *stop) {
+            NSMutableString *newString = [NSMutableString stringWithString:string];
+            // Iterate over the array of vowels, replacing occurrences of each
+            // with an empty string
+            for (NSString *s in vowels) {
+                NSRange fullRange = NSMakeRange(0, [newString length]);
+                [newString replaceOccurrencesOfString:s withString:@""
+                                              options:NSCaseInsensitiveSearch
+                                                range:fullRange];
+            }
+            [devowelizedStrings addObject:newString];
+        }; // End of block statement
     }
     return 0;
 }
