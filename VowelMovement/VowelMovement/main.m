d3da6dab2808e1f694646dbfda5e7aab5396e5a3
Managam Silalahi <managam.silalahi@gmail.com>
2016-09-05 19:04:47 +0700

    Add original strings

--- VowelMovement/VowelMovement/main.m
+++ VowelMovement/VowelMovement/main.m
@@ -11,7 +11,16 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        // NSLog(@"Hello, World!");
+        
+        // Create array of strings and a container for devowelized ones
+        NSArray *originalStrings = @[@"Sauerkraut", @"Raygun",
+                                      @"Big Nerd Ranch", @"Mississippi"];
+        NSLog(@"Original strings: %@", originalStrings);
+        
+        NSMutableArray *devowelizedStrings = [NSMutableArray array];
+        // Create a list of characters to be removed from the string
+        NSArray *vowels = @[@"a", @"e", @"i", @"o", @"u"];
     }
     return 0;
 }
