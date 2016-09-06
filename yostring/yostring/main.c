1d4af8c05deb5d7d9ecc37f7ac3f0ce2c5077892
Managam Silalahi <managam.silalahi@gmail.com>
2016-09-06 14:48:22 +0700

    Add project C: yostring

---
+++ yostring/yostring/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  yostring
+//
+//  Created by Managam Silalahi on 9/6/16.
+//  Copyright © 2016 Managam Silalahi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
